ba985ac4393022b46704fafaec46109d7c3fcdf1
nenad <neno.nenadovic@gmail.com>
2018-12-16 19:39:58 +0100

    add component assignment

---
+++ Start/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="container">
+        <header-component>
+        </header-component>
+        <hr>
+        <server-details-component>
+        </server-details-component>
+        <hr>
+        <footer-component>
+        </footer-component>
+    </div>
+</template>
+
+<script>
+    import HeaderComponent from './Header.vue'
+    import DetailsComponent from './Details.vue'
+    import FooterComponent from './Footer.vue'
+
+    export default {
+        components: {
+            'header-component': HeaderComponent,
+            'server-details-component': DetailsComponent,
+            'footer-component': FooterComponent
+
+        }
+    }
+</script>
+
+<style>
+
+</style>
