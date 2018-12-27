d3209e2c006295b26ba0ba3451a1b4120694d845
nenad <neno.nenadovic@gmail.com>
2018-12-27 09:58:30 +0100

    animations assgnment initial commit

---
+++ calculatingGame/src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-8 col-md-offset-2">
+        <br>
+        <br>
+        <h1 class="text-center">The Super Quiz</h1>
+        <hr>
+      </div>
+    </div>
+
+    <div class="row">
+      <question></question>
+    </div>
+
+  </div>
+</template>
+
+<script>
+  import Question from './components/Question'
+
+  export default {
+    name: 'app',
+    components: {
+      question: Question
+    },
+    data() {
+      return {
+        msg: 'Welcome to Your Vue.js App'
+      }
+    }
+  }
+</script>
+
+<style>
+
+</style>
---
+++ calculatingGame/src/components/Question.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="col-xs-6 col-sm-4 col-sm-offset-4 col-md-4 col-md-offset-4">
+    <div class="panel panel-success">
+      <div class="panel-heading text-center">What is {{question}}</div>
+      <div class="panel-body">
+        <div class="row">
+          <div class="col-xs-6 text-center">
+            <button class="btn btn-primary">{{answers[0]}}</button>
+          </div>
+          <div class="col-xs-6 text-center">
+            <button class="btn btn-primary">{{answers[1]}}</button>
+          </div>
+        </div>
+        <br>
+        <div class="row">
+          <div class="col-xs-6 text-center">
+            <button class="btn btn-primary">{{answers[2]}}</button>
+          </div>
+          <div class="col-xs-6 text-center">
+            <button class="btn btn-primary">{{answers[3]}}</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    beforeMount: function () {
+      this.prepareNextQuestion()
+    },
+    data() {
+      return {
+        a: null,
+        b: null,
+        answer: null,
+        question: '',
+        answers: ['A', 'B', '3', 'D']
+      }
+    },
+    methods: {
+      prepareNextQuestion() {
+        this.a = Math.floor(Math.random() * 101) - 50
+        this.b = Math.floor(Math.random() * 101) - 50
+        this.answer = this.a + this.b
+
+        if (this.b >= 0) {
+          this.question = this.a + ' + ' + this.b
+        } else {
+          this.question = this.a + ' - ' + (-this.b)
+        }
+
+        var i
+        for (i = 0; i < 4; i++) {
+          this.answers[i] = this.answer + (Math.floor(Math.random() * 5) - 2)
+        }
+
+        this.answers[Math.floor(Math.random() * 4)] = this.answer
+      }
+    }
+  }
+</script>
+
+<style>
+
+</style>
